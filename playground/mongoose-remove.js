8fc8cb9918d194ad7d582a2b608013091593fc05
Hailey Bae <hwisunbae.hb@gmail.com>
2018-11-29 16:25:38 -0500

    Add DELETE /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,9 +4,17 @@ const {mongoose} = require('../server/db/mongoose');
 const {Todo} = require('../server/models/todo');
 const {User} = require('../server/models/user');
 
-Todo.remove({}).then((res) => {
-    console.log(res);
-});
+// Todo.remove({}).then((res) => {
+//     console.log(res);
+// });
 
 // Todo.findOneAndRemove()
 // Todo.findByIdAndRemove()
+
+Todo.findOneAndRemove({_id : '5c00311b504b2f3bdd5c0b60'}).then((todos) => {
+
+});
+
+Todo.findByIdAndRemove('5c00311b504b2f3bdd5c0b60').then((todos) => {
+    console.log(todos);
+});
