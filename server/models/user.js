80af42b78717e88326bf8c382f07a09b87cc53c1
Hailey Bae <hwisunbae.hb@gmail.com>
2018-12-03 11:26:14 -0500

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -60,6 +60,27 @@ UserSchema.methods.generateAuthToken = function (){
     });
 };
 
+UserSchema.statics.findByToken = function(token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    }
+    catch (e) {
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // })
+        return Promise.reject('x-auth value uncorrect');
+    }
+    
+    return User.findOne({
+        '_id' : decoded._id,
+        'tokens.token' : token,
+        'tokens.access' : 'auth'
+    });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 // var user = new User({
